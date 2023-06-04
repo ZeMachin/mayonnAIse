1ab7e2f9f8c1d8986b488fe91fbd997d011450d9
Sinouhé <sinouhes@gmail.com>
2023-06-04 17:39:15 +0200

    feat: error handling

--- backend/interpreter/controller.ts
+++ backend/interpreter/controller.ts
@@ -1,40 +1,48 @@
 import { Request, Response, NextFunction } from 'express';
 import needle from "needle";
+import util from "util"
 import { Category, CategoryProperty } from './model/category.model';
 import { CATEGORIES } from './data/categories';
 import { DTO } from './model/dto.model';
 
 const interpreter = async (req: Request, res: Response, next: NextFunction) => {
-  console.log('interpreter()');
-  console.log('query:', req.body.query)
-
-  const originalQuery: string = req.body.query;
-  const pickedData = originalQuery.split(' ');
-
-  const chosenCategory: Category = pickCategory(parseInt(pickedData[0]));
-  const chosenProperty: CategoryProperty = pickProperty(chosenCategory, parseInt(pickedData[1]));
-
-  const query = `
-      query Query {
-        ${chosenCategory.root} {
-          ${chosenCategory.firstNode} {
-            ${chosenCategory.firstNode === 'films' ? 'title' : 'name'}
-            ${chosenProperty.request}
+  try {
+    console.log('interpreter()');
+    // console.log('body:', util.inspect(req.body))
+    console.log('query:', req.body.query)
+  
+    const originalQuery: string = req.body.query;
+    const pickedData = originalQuery.split(' ');
+  
+    const chosenCategory: Category = pickCategory(parseInt(pickedData[0]));
+    const chosenProperty: CategoryProperty = pickProperty(chosenCategory, parseInt(pickedData[1]));
+  
+    const query = `
+        query Query {
+          ${chosenCategory.root} {
+            ${chosenCategory.firstNode} {
+              ${chosenCategory.firstNode === 'films' ? 'title' : 'name'}
+              ${chosenProperty.request}
+            }
           }
         }
+      `
+  
+    const data = { query: query };
+  
+    needle.post('https://swapi-graphql.netlify.app/.netlify/functions/index', data, (error, response, body) => {
+      console.log('response:', response.body)
+      if (body?.data) {
+        const dto: DTO = { data: body.data, chart: chosenProperty.chart, title: `${chosenCategory.firstNode} ${chosenProperty.name}`, category: chosenCategory, property: chosenProperty, query: originalQuery }
+        res.send(dto);
       }
-    `
-
-  const data = { query: query };
-
-  needle.post('https://swapi-graphql.netlify.app/.netlify/functions/index', data, (error, response, body) => {
-    console.log('response:', response.body)
-    if (body?.data) {
-      const dto: DTO = { data: body.data, chart: chosenProperty.chart, title: `${chosenCategory.firstNode} ${chosenProperty.name}`, category: chosenCategory, property: chosenProperty, query: originalQuery }
-      res.send(dto);
-    }
-    else res.send('error')
-  })
+      else res.send('error')
+    })
+  } catch (err: any) {
+    console.error('err:', err)
+    res.status(500).send(`error: ${err}`)
+  }
+  
 }
 
 const pickCategory = (idx?: number): Category => {
--- backend/interpreter/routes.ts
+++ backend/interpreter/routes.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import controller from './controller';
 const router = express.Router();
 
-router.post("/interpreter", (req, res, next) => controller.interpreter);
-// router.post("/api/interpreter", (req, res, next) => controller.interpreter);
 router.post("/api/interpreter", (req, res, next) =>
     controller.interpreter(req, res, next)
 );
+
 router.get("*", (req, res, next) => {
     console.log('connection attempt')
+    console.log('url:', req.originalUrl)
     res.status(404).json({ message: 'error 404' })
 })
 
