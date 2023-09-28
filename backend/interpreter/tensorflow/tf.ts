fe413f605bbe25f86587765f6e439d5f7142630e
Sinouhé <sinouhes@gmail.com>
2023-09-28 12:19:36 +0200

    feat: feedback function

--- backend/interpreter/tensorflow/tf.ts
+++ backend/interpreter/tensorflow/tf.ts
@@ -5,11 +5,6 @@ import { CATEGORIES } from '../data/categories';
 // import tf from '@tensorflow/tfjs-node-gpu';
 const MODEL_STORAGE_PATH = 'file://./tensorflow/tf-model';
 const NUMBER_EPOCHS = 10;
-const MAX_FEATURES = 10000;
-const SEQUENCE_LENGTH = 25;
-const LEARNING_RATE = 3e-4;
-const TOKENIZER = {};
-const VOCAB_PATH = 'file://./tensorflow/vocab/dictonary.json';
 
 const loadOrCreateModel = async (modelName: string, pseudoClasses: number) => {
     let model = await loadModel(modelName);
@@ -22,77 +17,12 @@ const loadOrCreateModel = async (modelName: string, pseudoClasses: number) => {
 }
 
 const createModel = async (pseudoClasses: number) => {
-    // Define our model.
-    const model = tf.sequential();
-
-    // // 1st layer: a 1d convolutional network
-    // model.add(tf.layers.conv1d({
-    //     filters: 100,
-    //     kernelSize: 3,
-    //     strides: 1,
-    //     activation: 'relu',
-    //     padding: 'valid',
-    //     inputShape: [MAX_FEATURES, SEQUENCE_LENGTH],
-    // }));
-
-    // // transform 2d input into 1d
-    // model.add(tf.layers.globalMaxPool1d({}));
-
-    // // the final layer with one neuron
-    // model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-
-    // // here are some tuning, read in the TF docs for more
-    // model.compile({
-    //     optimizer: tf.train.adam(LEARNING_RATE),
-    //     loss: 'binaryCrossentropy',
-    //     metrics: ['accuracy'],
-    // });
-
-    model.add(tf.layers.dense({ units: 100, inputShape: [SEQUENCE_LENGTH] }));
-    model.add(tf.layers.activation({ activation: 'softmax' }));
-    model.add(tf.layers.dense({ units: 1 }));
-    model.compile({ loss: 'categoricalCrossentropy', optimizer: tf.train.sgd(0.001), metrics: ['accuracy'] });
-
-    // print the model architecture
-    model.summary();
+    // TODO: Call python service through Rest API
+    const model = false;
     return model;
 }
 
-const tokenize = (text: string) => {
-    text = text.toLowerCase();
-    text = text.replace(/[!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n]/g, '');
-    const split_text = text.split('');
-    const tokens: string[] = [];
-    split_text.forEach((element: string) => {
-        if (TOKENIZER[element as keyof typeof TOKENIZER] != undefined) {
-            tokens.push(TOKENIZER[element as keyof typeof TOKENIZER]);
-        }
-    });
-
-    // create a list of slices of the list of tokens
-    let i = 0;
-    const tokenized_text_segments = [];
-    while (i + 50 < Math.max(tokens.length, 100)) {
-        const new_slice = tokens.slice(i, i + 100);
-        while (new_slice.length < 100) {
-            new_slice.push('');
-        }
-        tokenized_text_segments.push(new_slice);
-        i = i + 50;
-    }
-    return tokenized_text_segments;
-}
-
-//TODO: load tokenizer on init, save after each call
-// load the tokenizer from json
-const loadTokenizer = () => {
-    let tokenizer = fetch(VOCAB_PATH).then(response => {
-        return response.json();
-    })
-    return tokenizer;
-}
-
-const trainModel = async (model: any, data1: any, data2: any) => {
+const trainModel = async (model: any, query: string, predicted: Category | CategoryProperty, correct: Category | CategoryProperty) => {
     // Train the model.
     console.log('using feedback to train model');
     await model.fit(data1, data1, {
@@ -139,7 +69,25 @@ const predictProperty = async (category: Category, query: string): Promise<Categ
 }
 
 const feedback = (query: string, categoryPredicted: Category, categoryPropertyPredicted: CategoryProperty, correct: boolean, correctCategory?: Category, correctCategoryProperty?: CategoryProperty) => {
-    // Use data to train model
+    // If prediction entirely correct
+    if(correct && !correctCategory && !correctCategoryProperty) { 
+        const modelCategories = loadOrCreateModel(`category-predicter`, CATEGORIES.length);
+        trainModel(modelCategories, query, categoryPredicted, categoryPredicted);
+        const modelProperties = loadOrCreateModel(`property-predicter/${categoryPredicted.name}`, categoryPredicted.properties.length);
+        trainModel(modelProperties, query, categoryPropertyPredicted, categoryPropertyPredicted);
+    }
+    // If category correct only
+    else if(!correct && correctCategory && categoryPredicted.name === correctCategory.name && correctCategoryProperty) {
+        const modelCategories = loadOrCreateModel(`category-predicter`, CATEGORIES.length);
+        trainModel(modelCategories, query, categoryPredicted, correctCategory);
+        const modelProperties = loadOrCreateModel(`property-predicter/${categoryPredicted.name}`, categoryPredicted.properties.length);
+        trainModel(modelProperties, query, categoryPropertyPredicted, correctCategoryProperty);
+    }
+    // If category incorrect
+    else if(!correct && correctCategory && categoryPredicted.name !== correctCategory.name) {
+        const modelCategories = loadOrCreateModel(`category-predicter`, CATEGORIES.length);
+        trainModel(modelCategories, query, categoryPredicted, correctCategory);
+    }
     // TODO: export data to make graphs
 }
 
