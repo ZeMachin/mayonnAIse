b9e5d51e19548e33f820f6f06b4786c7756411e0
Sinouhé <sinouhes@gmail.com>
2023-09-28 12:00:41 +0200

    feat: using tf module to pick categories/properties

--- backend/interpreter/controller.ts
+++ backend/interpreter/controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import needle from "needle";
-import util from "util"
 import { Category, CategoryProperty } from './model/category.model';
 import { CATEGORIES } from './data/categories';
 import { DTO } from './model/dto.model';
@@ -13,10 +12,9 @@ const interpreter = async (req: Request, res: Response, next: NextFunction) => {
     console.log('query:', req.body.query)
   
     const originalQuery: string = req.body.query;
-    const pickedData = originalQuery.split(' ');
   
-    const chosenCategory: Category = pickCategory(parseInt(pickedData[0]));
-    const chosenProperty: CategoryProperty = pickProperty(chosenCategory, parseInt(pickedData[1]));
+    const chosenCategory: Category = predictCategory(originalQuery);
+    const chosenProperty: CategoryProperty = predictProperty(chosenCategory, originalQuery);
   
     const query = `
         query Query {
@@ -43,20 +41,53 @@ const interpreter = async (req: Request, res: Response, next: NextFunction) => {
     console.error('err:', err)
     res.status(500).send(`error: ${err}`)
   }
-  
 }
 
-const pickCategory = (idx?: number): Category => {
-  return CATEGORIES[idx ?? Math.floor(Math.random() * CATEGORIES.length)];
+const predictCategory = (query: string): Category => {
+  return tf.predictCategory(query);
 }
 
-const pickProperty = (category: Category, idx?: number): CategoryProperty => {
-  return category.properties[idx ?? Math.floor(Math.random() * category.properties.length)];
+const predictProperty = (category: Category, query: string): CategoryProperty => {
+  return tf.predictProperty(category, query);
 }
 
 const test = async (req: Request, res: Response, next: NextFunction) => {
-  await tf.test();
-  res.send('it worked =)');
+  try {
+    console.log('interpreter()');
+    // console.log('body:', util.inspect(req.body))
+    console.log('query:', req.body.query)
+  
+    const originalQuery: string = req.body.query;
+    const pickedData = originalQuery.split(' ');
+  
+    const chosenCategory: Category = CATEGORIES[parseInt(pickedData[0])];
+    const chosenProperty: CategoryProperty = chosenCategory.properties[(parseInt(pickedData[1]))];
+  
+    const query = `
+        query Query {
+          ${chosenCategory.root} {
+            ${chosenCategory.firstNode} {
+              ${chosenCategory.firstNode === 'films' ? 'title' : 'name'}
+              ${chosenProperty.request}
+            }
+          }
+        }
+      `
+  
+    const data = { query: query };
+  
+    needle.post('https://swapi-graphql.netlify.app/.netlify/functions/index', data, (error, response, body) => {
+      console.log('response:', response.body)
+      if (body?.data) {
+        const dto: DTO = { data: body.data, chart: chosenProperty.chart, title: `${chosenCategory.firstNode} ${chosenProperty.name}`, category: chosenCategory, property: chosenProperty, query: originalQuery }
+        res.send(dto);
+      }
+      else res.send('error')
+    })
+  } catch (err: any) {
+    console.error('err:', err)
+    res.status(500).send(`error: ${err}`)
+  }
 }
 
 export default { interpreter, test }
--- backend/interpreter/tensorflow/tf.ts
+++ backend/interpreter/tensorflow/tf.ts
@@ -1,54 +1,146 @@
 import * as tf from '@tensorflow/tfjs-node';
+import { Category, CategoryProperty } from '../model/category.model';
+import { CATEGORIES } from '../data/categories';
 // Use `tfjs-node-gpu`. Note that `tfjs` is imported indirectly by `tfjs-node-gpu`.
 // import tf from '@tensorflow/tfjs-node-gpu';
 const MODEL_STORAGE_PATH = 'file://./tensorflow/tf-model';
+const NUMBER_EPOCHS = 10;
+const MAX_FEATURES = 10000;
+const SEQUENCE_LENGTH = 25;
+const LEARNING_RATE = 3e-4;
+const TOKENIZER = {};
+const VOCAB_PATH = 'file://./tensorflow/vocab/dictonary.json';
 
-const test = async () => {
-    let created = false;
-    let model = await loadModel();
+const loadOrCreateModel = async (modelName: string, pseudoClasses: number) => {
+    let model = await loadModel(modelName);
     if (!model) {
-        created = true;
         console.log('no model found, creating model...')
-        model = await createModel();
+        model = await createModel(pseudoClasses);
+        await saveModel(model, modelName);
     }
-    console.log(`model ${created ? 'created' : 'found'}, saving model...`);
-    await saveModel(model);
-    console.log('model saved');
+    return model;
 }
 
-const createModel = async () => {
-    // Define a simple model.
+const createModel = async (pseudoClasses: number) => {
+    // Define our model.
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 100, activation: 'relu', inputShape: [10] }));
-    model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
-    model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
 
-    const xs = tf.randomNormal([100, 10]);
-    const ys = tf.randomNormal([100, 1]);
+    // // 1st layer: a 1d convolutional network
+    // model.add(tf.layers.conv1d({
+    //     filters: 100,
+    //     kernelSize: 3,
+    //     strides: 1,
+    //     activation: 'relu',
+    //     padding: 'valid',
+    //     inputShape: [MAX_FEATURES, SEQUENCE_LENGTH],
+    // }));
 
+    // // transform 2d input into 1d
+    // model.add(tf.layers.globalMaxPool1d({}));
+
+    // // the final layer with one neuron
+    // model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+
+    // // here are some tuning, read in the TF docs for more
+    // model.compile({
+    //     optimizer: tf.train.adam(LEARNING_RATE),
+    //     loss: 'binaryCrossentropy',
+    //     metrics: ['accuracy'],
+    // });
+
+    model.add(tf.layers.dense({ units: 100, inputShape: [SEQUENCE_LENGTH] }));
+    model.add(tf.layers.activation({ activation: 'softmax' }));
+    model.add(tf.layers.dense({ units: 1 }));
+    model.compile({ loss: 'categoricalCrossentropy', optimizer: tf.train.sgd(0.001), metrics: ['accuracy'] });
+
+    // print the model architecture
+    model.summary();
+    return model;
+}
+
+const tokenize = (text: string) => {
+    text = text.toLowerCase();
+    text = text.replace(/[!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n]/g, '');
+    const split_text = text.split('');
+    const tokens: string[] = [];
+    split_text.forEach((element: string) => {
+        if (TOKENIZER[element as keyof typeof TOKENIZER] != undefined) {
+            tokens.push(TOKENIZER[element as keyof typeof TOKENIZER]);
+        }
+    });
+
+    // create a list of slices of the list of tokens
+    let i = 0;
+    const tokenized_text_segments = [];
+    while (i + 50 < Math.max(tokens.length, 100)) {
+        const new_slice = tokens.slice(i, i + 100);
+        while (new_slice.length < 100) {
+            new_slice.push('');
+        }
+        tokenized_text_segments.push(new_slice);
+        i = i + 50;
+    }
+    return tokenized_text_segments;
+}
+
+//TODO: load tokenizer on init, save after each call
+// load the tokenizer from json
+const loadTokenizer = () => {
+    let tokenizer = fetch(VOCAB_PATH).then(response => {
+        return response.json();
+    })
+    return tokenizer;
+}
+
+const trainModel = async (model: any, data1: any, data2: any) => {
     // Train the model.
-    await model.fit(xs, ys, {
-        epochs: 10,
+    console.log('using feedback to train model');
+    await model.fit(data1, data1, {
+        epochs: NUMBER_EPOCHS,
         callbacks: {
             onEpochEnd: (epoch: any, log: any) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
         }
     });
-
-    return model;
 }
 
-const saveModel = async (model: any) => {
-    await model.save(MODEL_STORAGE_PATH);
-}
-
-const loadModel = async () => {
-    console.log('loading model...');
+const saveModel = async (model: any, modelName: string) => {
+    console.log('saving model:', modelName);
     try {
-        return await tf.loadLayersModel(MODEL_STORAGE_PATH + '/model.json');
+        await model.save(`${MODEL_STORAGE_PATH}/${modelName}`);
     } catch (err) {
-        console.error(err); 
+        console.error(err);
+    }
+}
+
+const loadModel = async (modelName: string) => {
+    console.log('loading model:', modelName);
+    try {
+        return await tf.loadLayersModel(`${MODEL_STORAGE_PATH}/${modelName}/model.json`);
+    } catch (err) {
+        console.error(err);
     }
     return false;
 }
 
-export { test }
+const predictCategory = async (query: string): Promise<Category> => {
+    const model = loadOrCreateModel('category-predicter', CATEGORIES.length);
+    // TODO: transform query to Tensor
+    const predictions = (await model).predict(tf.linspace(0, 1, 100));
+    // TODO: return most likely class
+    // TODO: show confidence in logs
+    return CATEGORIES[0];
+}
+
+const predictProperty = async (category: Category, query: string): Promise<CategoryProperty> => {
+    const model = loadOrCreateModel(`property-predicter/${category.name}`, category.properties.length);
+    // Get a prediction based on category & query 
+    // Return prediction
+    return CATEGORIES[0].properties[0];
+}
+
+const feedback = (query: string, categoryPredicted: Category, categoryPropertyPredicted: CategoryProperty, correct: boolean, correctCategory?: Category, correctCategoryProperty?: CategoryProperty) => {
+    // Use data to train model
+    // TODO: export data to make graphs
+}
+
+export { predictCategory, predictProperty }
